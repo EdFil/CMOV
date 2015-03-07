03d2b05d4fb8108d46d0e5756d5dbc50220449c3
Edgar Filipe Ramires dos Santos <edfil221@gmail.com>
2015-03-07 13:06:25 +0000

    Now its finished, not before

--- LAB2/LAB2-EX4/app/src/main/java/pt/ulisboa/tecnico/cmov/lab2_ex4/CreateNoteActivity.java
+++ LAB2/LAB2-EX4/app/src/main/java/pt/ulisboa/tecnico/cmov/lab2_ex4/CreateNoteActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import pt.ulisboa.tecnico.cmov.lab2_ex4.note.Note;
 
@@ -15,6 +16,7 @@ import pt.ulisboa.tecnico.cmov.lab2_ex4.note.Note;
 public class CreateNoteActivity extends Activity {
 
     private final static String TAG = CreateNoteActivity.class.getSimpleName();
+    private final static String SAVED_NOTE_TEXT = "We have saved a note for you";
 
     TextView mTitle;
     TextView mDescription;
@@ -26,7 +28,39 @@ public class CreateNoteActivity extends Activity {
 
         mTitle = (TextView) findViewById(R.id.noteTitle);
         mDescription = (TextView) findViewById(R.id.noteDescription);
+
+        if(savedInstanceState != null){
+            Note note = savedInstanceState.getParcelable(Note.NOTE_MESSAGE);
+            if(note != null){
+                Toast.makeText(getApplicationContext(), SAVED_NOTE_TEXT, Toast.LENGTH_SHORT);
+                mTitle.setText(note.getTitle());
+                mDescription.setText(note.getDescription());
+            }
+        }
     }
+
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if(savedInstanceState != null){
+            Note note = savedInstanceState.getParcelable(Note.NOTE_MESSAGE);
+            if(note != null){
+                Log.d(TAG, "Restored a note");
+                mTitle.setText(note.getTitle());
+                mDescription.setText(note.getDescription());
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putParcelable(Note.NOTE_MESSAGE, new Note(mTitle.getText().toString(), mDescription.getText().toString()));
+        Log.d(TAG, "Saved a note");
+    }
+
 
     public void onClickOk(View view){
         Intent intent = getIntent();
