3e0324ce71c5daed68edc07b647a5c67a4ce2fb5
Edgar Filipe Ramires dos Santos <edfil221@gmail.com>
2015-04-02 20:36:03 +0100

    Started details activity

--- Proj/AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/core/tag/Tag.java
+++ Proj/AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/core/tag/Tag.java
@@ -1,11 +1,15 @@
 package pt.ulisboa.tecnico.cmov.airdesk.core.tag;
 
+import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import pt.ulisboa.tecnico.cmov.airdesk.core.workspace.Workspace;
 
 /**
  * Created by edgar on 31-03-2015.
  */
-public class Tag {
+public class Tag implements Parcelable {
 
     private Workspace mWorkspace;
     private String mText;
@@ -28,8 +32,36 @@ public class Tag {
         mWorkspace = workspace;
     }
 
-    @Override
-    public String toString(){
-        return "Tag '" + mText + "'.";
+
+
+
+    // ---------------------
+    // Stuff for Parcelable
+    // ---------------------
+
+
+    private Tag(Parcel in){
+        mText = in.readString();
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mText);
+    }
+
+    // this is used to regenerate your object. All Parcelables must have a CREATOR that implements these two methods
+    public static final Parcelable.Creator<Tag> CREATOR = new Parcelable.Creator<Tag>() {
+        public Tag createFromParcel(Parcel in) {
+            return new Tag(in);
+        }
+
+        public Tag[] newArray(int size) {
+            return new Tag[size];
+        }
+    };
 }
