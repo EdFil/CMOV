b9fdd334df0520d00a039baf60294b54bf7123cc
BFips <brunol1991@hotmail.com>
2015-04-09 18:19:20 +0100

    Details and Edit: paddings, add tags, update views TODO: remove tags

--- Proj/AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/fragment/WorkspacesDetailsEditFragment.java
+++ Proj/AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/fragment/WorkspacesDetailsEditFragment.java
@@ -3,23 +3,36 @@ package pt.ulisboa.tecnico.cmov.airdesk.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import pt.ulisboa.tecnico.cmov.airdesk.R;
+import pt.ulisboa.tecnico.cmov.airdesk.core.tag.Tag;
 import pt.ulisboa.tecnico.cmov.airdesk.core.user.User;
 import pt.ulisboa.tecnico.cmov.airdesk.core.workspace.Workspace;
+import pt.ulisboa.tecnico.cmov.airdesk.core.workspace.WorkspaceManager;
 
 public class WorkspacesDetailsEditFragment extends Fragment {
 
+    private static final int NUM_COLUMNS_PER_ROW = 4;
+
     EditText mNameInformationEdit;
-    EditText mOwnerInformationEdit;
     EditText mQuotaInformationEdit;
     Switch mPrivacyInformationSwitch;
+    TextView mPrivateInformationText;
+    TextView mPublicInformationText;
+    EditText mNewTagInformationEdit;
+
+    Button mAddTagButtonEdit;
 
     TableLayout mTagsTableLayout;
     TableLayout mUsersTableLayout;
@@ -36,31 +49,74 @@ public class WorkspacesDetailsEditFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View workspaceFragmentView = inflater.inflate(R.layout.fragment_workspace_details_edit, container, false);
 
+        // Find Views
         mNameInformationEdit = (EditText) workspaceFragmentView.findViewById(R.id.nameInformationEdit);
-        mOwnerInformationEdit = (EditText) workspaceFragmentView.findViewById(R.id.ownerInformationEdit);
         mQuotaInformationEdit = (EditText) workspaceFragmentView.findViewById(R.id.quotaInformationEdit);
         mPrivacyInformationSwitch = (Switch) workspaceFragmentView.findViewById(R.id.privateInformationSwitch);
+        mPrivateInformationText = (TextView) workspaceFragmentView.findViewById(R.id.privateInformationText);
+        mPublicInformationText = (TextView) workspaceFragmentView.findViewById(R.id.publicInformationText);
+
+        mNewTagInformationEdit = (EditText) workspaceFragmentView.findViewById(R.id.newTagInformationEdit);
+        mAddTagButtonEdit = (Button) workspaceFragmentView.findViewById(R.id.addTagButtonEdit);
+        mAddTagButtonEdit.setVisibility(mWorkspace.isPrivate() ? View.INVISIBLE : View.VISIBLE);
+        mNewTagInformationEdit.setVisibility(mWorkspace.isPrivate() ? View.INVISIBLE : View.VISIBLE);
 
         mTagsTableLayout = (TableLayout) workspaceFragmentView.findViewById(R.id.tagsTable);
         mTagsTableLayout.setStretchAllColumns(true);
+
         mUsersTableLayout = (TableLayout) workspaceFragmentView.findViewById(R.id.usersTable);
         mUsersTableLayout.setStretchAllColumns(true);
 
+        // Set initial values
         mNameInformationEdit.setText(mWorkspace.getName());
-        mOwnerInformationEdit.setText(mWorkspace.getOwner().getNick());
         mQuotaInformationEdit.setText(String.valueOf(mWorkspace.getQuota()));
         mPrivacyInformationSwitch.setChecked(!mWorkspace.isPrivate());
 
-//        for(Tag tag : workspace.getTags())
-//            addTagToTable(tag.getText());
-//
-//        for(User user : workspace.getUsers())
-//            addUserToTable(user.getEmail());
+        for(Tag tag : mWorkspace.getTags())
+            addTagToTable(tag.getText());
 
+        for(User user : mWorkspace.getUsers())
+            addUserToTable(user.getEmail());
+
+        // Set Listeners
+        mPrivacyInformationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mAddTagButtonEdit.setVisibility(isChecked ? View.VISIBLE : View.INVISIBLE);
+                mNewTagInformationEdit.setVisibility(isChecked ? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        mPublicInformationText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPrivacyInformationSwitch.setChecked(true);
+            }
+        });
+
+        mPrivateInformationText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPrivacyInformationSwitch.setChecked(false);
+            }
+        });
+
+        mAddTagButtonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String tag = mNewTagInformationEdit.getText().toString().trim();
+
+                if (tag.length() > 0) {
+                    addTagToTable(tag);
+                    WorkspaceManager.getInstance().addTagToWorkspace(tag, mWorkspace);
+                    mNewTagInformationEdit.getText().clear();
+                }
+            }
+        });
 
         return workspaceFragmentView;
     }
-
 
     @Override
     public void onAttach(Activity activity) {
@@ -72,9 +128,40 @@ public class WorkspacesDetailsEditFragment extends Fragment {
     }
 
     public Workspace getEditedWorkspace() {
-        mWorkspace.setName(mNameInformationEdit.getText().toString());
-        mWorkspace.setQuota(Long.parseLong(mQuotaInformationEdit.getText().toString().trim()));
-        mWorkspace.setIsPrivate(!mPrivacyInformationSwitch.isChecked());
+        String newWorkspaceName = mNameInformationEdit.getText().toString();
+        Long newWorkspaceQuota = Long.parseLong(mQuotaInformationEdit.getText().toString().trim());
+        boolean newWorkspacePrivacy = !mPrivacyInformationSwitch.isChecked();
+        WorkspaceManager.getInstance().updateWorkspace(mWorkspace, newWorkspaceName, newWorkspaceQuota, newWorkspacePrivacy);
         return mWorkspace;
+    }
+
+    private void addTagToTable(String tag) {
+        TextView tagText = new TextView(getActivity());
+        tagText.setText(tag);
+        tagText.setTextAppearance(getActivity(), android.R.style.TextAppearance_DeviceDefault_Medium);
+        TableRow rowToInsertTag;
+        int numRows = mTagsTableLayout.getChildCount();
+        if(numRows == 0 || ((TableRow)mTagsTableLayout.getChildAt(numRows - 1)).getChildCount() >= NUM_COLUMNS_PER_ROW) {
+            rowToInsertTag = new TableRow(getActivity());
+            mTagsTableLayout.addView(rowToInsertTag);
+        } else {
+            rowToInsertTag = (TableRow)mTagsTableLayout.getChildAt(numRows - 1);
+        }
+        rowToInsertTag.addView(tagText, ActionBar.LayoutParams.WRAP_CONTENT);
+    }
+
+    private void addUserToTable(String tag) {
+        TextView userText = new TextView(getActivity());
+        userText.setText(tag);
+        userText.setTextAppearance(getActivity(), android.R.style.TextAppearance_DeviceDefault_Medium);
+        TableRow rowToInsertTag;
+        int numRows = mUsersTableLayout.getChildCount();
+        if(numRows == 0 || ((TableRow)mUsersTableLayout.getChildAt(numRows - 1)).getChildCount() >= NUM_COLUMNS_PER_ROW) {
+            rowToInsertTag = new TableRow(getActivity());
+            mUsersTableLayout.addView(rowToInsertTag);
+        } else {
+            rowToInsertTag = (TableRow)mUsersTableLayout.getChildAt(numRows - 1);
+        }
+        rowToInsertTag.addView(userText, ActionBar.LayoutParams.WRAP_CONTENT);
     }
 }
