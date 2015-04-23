f7d4aa025f582c311827e3edaf04b189f84b42bf
Stevens <stevens.d2m@gmail.com>
2015-04-23 13:18:27 +0100

    LAB5 Working for Windows

--- LAB5/TermiteCMov/Termite-Cli/etc/env/env_laptop.sh
+++ LAB5/TermiteCMov/Termite-Cli/etc/env/env_laptop.sh
@@ -2,4 +2,5 @@
 #
 # Replace it with the right location
 
-export TERMITE_CLI_PATH=/d/Repositories/CMOV/LAB5/TermiteCMov/Termite-Cli
+export TERMITE_CLI_PATH=D:\\Repositories\\CMOV\\LAB5\\TermiteCMov\\Termite-Cli
+#export TERMITE_CLI_PATH=/d/Repositories/CMOV/LAB5/TermiteCMov/Termite-Cli
--- LAB5/TermiteCMov/Termite-Cli/termite.sh
+++ LAB5/TermiteCMov/Termite-Cli/termite.sh
@@ -6,9 +6,14 @@ then
 	exit -1
 fi
 
-jline=$TERMITE_CLI_PATH/libs/jline-2.13.jar
-groovy=$TERMITE_CLI_PATH/libs/groovy-all-2.3.6.jar
-termite=$TERMITE_CLI_PATH/build/libs/Termite-Cli.jar
-deps="$jline:$groovy:$termite"
+jline=$TERMITE_CLI_PATH\\libs\\jline-2.13.jar
+groovy=$TERMITE_CLI_PATH\\libs\\groovy-all-2.3.6.jar
+termite=$TERMITE_CLI_PATH\\build\\libs\\Termite-Cli.jar
 
+#jline=$TERMITE_CLI_PATH/libs/jline-2.13.jar
+#groovy=$TERMITE_CLI_PATH/libs/groovy-all-2.3.6.jar
+#termite=$TERMITE_CLI_PATH/build/libs/Termite-Cli.jar
+
+deps="$jline;$groovy;$termite"
+echo $deps
 java -cp $deps pt.inesc.termite.cli.Main
